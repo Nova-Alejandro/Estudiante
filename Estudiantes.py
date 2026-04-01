6795bc0b09ee7c18d16962ee9d7b814c51b47a59
Nova-Alejandro <david.alejandro.bn.097@gmail.com>
2026-04-01 09:34:44 -0500

    Update Estudiantes.py

--- Estudiantes.py
+++ Estudiantes.py
@@ -58,19 +58,49 @@ def delete_student():
 
     if not found:
         print("Student not found")
+def update_student():
+    try:
+        update_id = int(input("Enter the student ID to update: "))
+        field = input("What do you want to update? (name/course): ").lower()
 
+        found = False
+
+        for student in students:
+            if update_id == student['id_student']:
+                found = True
+
+                if field == "name":
+                    new_name = input("Enter new name: ")
+                    student['name'] = new_name
+                    print("Name updated successfully")
+
+                elif field == "course":
+                    new_course = input("Enter new course: ")
+                    student['course'] = new_course
+                    print("Course updated successfully")
+
+                else:
+                    print("Invalid option")
+
+                break
+
+        if not found:
+            print("Student not found")
+
+    except:
+        print("Error: invalid input")
 
 while True:
     option = input("""
 ================================
-    What do you want to do?
-
+What do you want to do?
     
 1.Add new students
 2.Show students
 3.Search student
 4.Delete a student
-5.Exit program
+5.update_student
+6.Exit program
 ================================
 
 Choose an option: """)
@@ -84,6 +114,8 @@ Choose an option: """)
     elif option == "4":
         delete_student()
     elif option == "5":
+        update_student()
+    elif option == "6":
         print("Exiting the program. Thanks for using it <3")
         break
     else:
